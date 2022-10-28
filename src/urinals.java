c974484a2903dca09c0713db2afc94a58e8eb124
charanbuddhavarapu <112653778+charanbuddhavarapu@users.noreply.github.com>
2022-10-27 22:57:10 -0700

    Commit-4

--- src/urinals.java
+++ src/urinals.java
@@ -1,11 +1,10 @@
 // Author : Devi Sricharan
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.*;
 
 public class urinals {
     public static int checking(char[] x) {
-        for (int i = 0; i < x.length; i++) {
+        for (int i = 0; i < x.length-1; i++) {
             if ((x[i] == '1') && (x[i + 1] == '1')) {
 
                 return -1;
@@ -20,15 +19,53 @@ public class urinals {
         return count;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the input string");
-        String inputs = sc.next();
-        Scanner filescan = new Scanner(new FileReader("Urinals.dat"));
+    public static void main(String[] args) throws IOException {
+        Scanner filescan = new Scanner(new FileReader("Urinals.dat")).useDelimiter("delimiter to separate things");
         String input = filescan.useDelimiter("\\A").next();
-        char[] s = input.toCharArray();
+        String[] str = input.split("\r\n");
+        FileWriter outfile = new FileWriter("rule1.txt");
+
+        for (int j = 0; j < str.length; j++) {
+            char[] s = str[j].toCharArray();
+            System.out.println(s);
+            int free = 0;
+            int a = urinals.checking(s);
+            if (a == -1) {
+
+                free = -1;
+            } else if (a == 0) {
+                for ( int i = 0; i < s.length; i++) {
+                    if (i == 0) {
+                        if (s[i] == '0' && s[i + 1] == '0') {
+                            s[i] = '1';
+                            free = counter(free);
+
+                        }
+
+                    } else if (i == s.length - 1) {
+                        if (s[i] == '0' && s[i - 1] == '0') {
+                            s[i] = '1';
+                            free = counter(free);
+                        }
 
 
+                    } else {
+                        if ((s[i - 1] == '0' && s[i] == '0' && s[i + 1] == '0')) {
+                            s[i] = '1';
+                            free = counter(free);
+
+                        }
+                    }
+                }
+            }
+            String out=String.valueOf(free);
+            outfile.write(out);
+            outfile.write("\n");
+            System.out.println(free);
+
+
+        }
+        outfile.close();
     }
 }
 
--- src/urinalsTest.java
+++ src/urinalsTest.java
@@ -4,6 +4,17 @@ class urinalsTest {
 
     @org.junit.jupiter.api.Test
     void checking() {
+        System.out.println("Charan Buddhavarapu--Test1 Implemented");
+        String x="01011";
+        String y="01000";
+        String d="111001";
+        String f="110001";
+        assertAll(() -> assertEquals(-1,urinals.checking(x.toCharArray())),
+                () -> assertEquals(-1,urinals.checking(y.toCharArray())),
+                () -> assertEquals(-1,urinals.checking(d.toCharArray())),
+                () -> assertEquals(-1,urinals.checking(f.toCharArray())));
+
+
     }
 
     @org.junit.jupiter.api.Test
@@ -12,5 +23,8 @@ class urinalsTest {
 
     @org.junit.jupiter.api.Test
     void main() {
+
     }
+
+
 }
