5a62af3bf3c93863f84826192189a5e8700c013a
charanbuddhavarapu <112653778+charanbuddhavarapu@users.noreply.github.com>
2022-10-27 19:08:54 -0700

    Commit-1

---
+++ src/urinals.java
@@ -0,0 +1,29 @@
+// Author : Devi Sricharan
+import java.io.FileReader;
+import java.util.*;
+
+public class urinals {
+    public static int checking(char[] x) {
+
+        return 0;
+    }
+
+    public static int counter(int count) {
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/urinalsTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @org.junit.jupiter.api.Test
+    void checking() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void counter() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
